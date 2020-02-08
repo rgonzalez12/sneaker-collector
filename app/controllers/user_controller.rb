7d7321df2ffc76dcc5a6d79791435b9bb87fcff5
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-02-08 21:05:54 +0000

    editing routes for sign-up and login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,21 +5,28 @@ class UserController < ApplicationController
   end
   
   post '/registrations/new' do
-    
+    if params[:first_name] == "" || params[:last_name] == "" || params[:email_address] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.new(:first_name => params[:first_name], :email_address => params[:email_address], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect ''
+    end
   end
   
   get '/login' do
     if !logged_in?
      erb :'/users/login'
     else
-     redirect '/users/home'
+     redirect '/users/:id/home'
     end
   end
   
   post '/login' do
-    login(params[:username], params[:password])
+    login(params[:email_address], params[:password])
     current_user
-    redirect to '/users/home'
+    redirect '/users/:id/home'
   end
   
   post '/logout' do
