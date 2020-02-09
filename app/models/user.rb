86472577f55cf106cca2480c7fdf32d601a37669
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-02-09 09:17:29 +0000

    added password requirements for user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+  
+  PASSWORD_REQ = /\A 
+  (?=.{8,})
+  (?=.*\d)
+  (?=.*[a-z])
+  (?=.*[A-Z])
+  (?=.*[[:^alnum:]])
+  /x
+  
   has_many :tweets
   has_secure_password
+  validates :password, format: PASSWORD_REQ
 end
