33f11bea4454e38b322a13a150daaf37a2bd55ff
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-02-10 00:15:53 +0000

    added placeholder associations

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,4 +1,20 @@
 class Sneaker < ActiveRecord::Base
-  belongs_to :portfolio
-  has_many :users
+  belongs_to :users
+  has_and_belongs_to_many :portfolio
 end
+
+
+#class Portfolio < ApplicationRecord
+#  has_many :sneakers
+#  has_one :user, through: :sneakers
+#end
+ 
+#class Sneaker < ApplicationRecord
+#  belongs_to :portfolio
+#  belongs_to :user
+#end
+ 
+#class User < ApplicationRecord
+#  has_many :sneakers
+#  has_one :portfolio, through: :sneakers
+#end
