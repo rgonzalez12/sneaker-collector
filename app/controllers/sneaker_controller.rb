b3b2692da9799b485638ed1644bd99ff46f8525b
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-02-17 03:22:45 +0000

    finalized logic for patch and delete route

--- app/controllers/sneaker_controller.rb
+++ app/controllers/sneaker_controller.rb
@@ -22,24 +22,24 @@ class SneakerController < ApplicationController
     if logged_in? && current_user == @sneaker.user
      erb :'/sneakers/edit_sneaker'
    else
-     redirect "/portfolio/#{current_user.portfolio.id}"
+     redirect "/portfolio/#{current_user.id}"
    end
   end
 
   patch '/portfolio/sneakers/:id' do
     if logged_in?
       if params[:manufacturer] == "" || params[:model] == "" || params[:size_us] == "" || params[:colorway] == "" || params[:est_value] == "" || params[:condition] == "" || params[:notes] == ""
-        redirect to "/shows/#{params[:id]}/edit"
+        redirect to "/portfolio/sneakers/#{params[:id]}/edit"
       else
-        @show = Show.find_by_id(params[:id])
-          if @show && @show.user == current_user
-            if @show.update(title: params[:title], rating: params[:rating], streaming_service: params[:streaming_service], review: params[:review])
-              redirect to "/shows/#{@show.id}"
+        @sneaker = Sneaker.find_by_id(params[:id])
+          if @sneaker && @sneaker.user == current_user
+            if @sneaker.update(manufacturer: params[:manufacturer], model: params[:model], size_us: params[:size_us], colorway: params[:colorway], condition: params[:condition], est_value: params[:est_value], notes: params[:notes])
+              redirect to "/portfolio/#{current_user.id}"
             else
-              redirect to "/shows/#{@shows.id}/edit"
+              redirect to "/portfolio/sneakers/#{params[:id]}/edit"
             end
           else
-            redirect to '/shows'
+            redirect to "/portfolio/#{current_user.id}"
           end
         end
       else
